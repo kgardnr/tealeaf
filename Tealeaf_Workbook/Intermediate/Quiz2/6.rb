79a0b29b8e8273bc7ddaabab96081f1beea738d3
Kim Gardner <kgardnr@gmail.com>
2015-06-03 02:15:39 -0400

    Exercise 6

---
+++ Tealeaf_Workbook/Intermediate/Quiz2/6.rb
@@ -0,0 +1,20 @@
+munsters = { 
+  "Herman" => { "age" => 32, "gender" => "male" }, 
+  "Lily" => { "age" => 30, "gender" => "female" }, 
+  "Grandpa" => { "age" => 402, "gender" => "male" }, 
+  "Eddie" => { "age" => 10, "gender" => "male" },
+  "Marilyn" => { "age" => 23, "gender" => "female"}
+}
+
+def mess_with_demographics(demo_hash)
+  demo_hash.values.each do |family_member|
+    family_member["age"] += 42
+    family_member["gender"] = "other"
+  end
+end
+
+mess_with_demographics(munsters)
+
+# Yes, because mess_with_demographics method is run on the munsters hash it will change the hash data
+
+p munsters.inspect
