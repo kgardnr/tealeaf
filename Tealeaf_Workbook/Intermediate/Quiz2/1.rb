7d5f0d802ecebb7879ff17e8d98ff72e8bef8075
Kim Gardner <kgardnr@gmail.com>
2015-06-03 01:16:29 -0400

    Exercise 1

--- Tealeaf_Workbook/Intermediate/Quiz2/1.rb
+++ Tealeaf_Workbook/Intermediate/Quiz2/1.rb
@@ -1,1 +1,18 @@
-#
+munsters = { 
+  "Herman" => { "age" => 32, "gender" => "male" }, 
+  "Lily" => { "age" => 30, "gender" => "female" }, 
+  "Grandpa" => { "age" => 402, "gender" => "male" }, 
+  "Eddie" => { "age" => 10, "gender" => "male" } 
+}
+
+total_male_age = 0
+
+munsters.each do |name, info| 
+  if info["gender"] == "male"
+    total_male_age += info["age"]
+  end
+end
+
+p total_male_age
+
+
